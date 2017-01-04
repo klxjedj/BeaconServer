e7b2a7270fef3046fa60d13998233e07601ae95a
klxjedj <mingchaowen@hotmail.com>
2017-01-04 12:52:14 +0800

    20170401

--- BeaconServer/BeaconServer/BeaconServer/model.py
+++ BeaconServer/BeaconServer/BeaconServer/model.py
@@ -16,7 +16,7 @@ def showItem(item):
 
 class Account(db.Model):
     __tablename__='Account'
-    id=db.Column(db.Integer,primary_key=True)
+    account_id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String,unique=True)
     password=db.Column(db.String)
     role=db.Column(db.String)
@@ -24,31 +24,34 @@ class Account(db.Model):
     def __repr__(self):
         return showItem(self)
 
+class Bidding(db.Model):
+    __tablename__='Bidding'
+    bidding_id=db.Column(db.Integer,primary_key=True)
+    record_id=db.Column(db.Integer,db.ForeignKey('CareRecord.record_id'))
+    caregiver_id=db.Column(db.Integer,db.ForeignKey('CareGiver.account_id'))
+    carerecipient_id=db.Column(db.Integer,db.ForeignKey('CareRecipient.account_id'))
+    bidding_status=db.Column(db.String)
+
 class CareGiver(db.Model):
     __tablename__='CareGiver'
-    id=db.Column(db.Integer,db.ForeignKey('Account.id'),primary_key=True)
-    account=db.relationship('Account')
-    
+    account_id=db.Column(db.Integer,db.ForeignKey('Account.account_id'),primary_key=True)
     name=db.Column(db.String)
+    gender=db.Column(db.String)
     email=db.Column(db.String)
     contact=db.Column(db.String)
     date_of_birth=db.Column(db.Date)
     highest_education_level=db.Column(db.String)
-   
+    no_of_service_given=db.Column(db.Integer)
     expertise=db.Column(db.String)
-
     block_status=db.Column(db.Integer)
     service_frequency=db.Column(db.Integer)
 
-    current_service=db.Column(db.Integer,db.ForeignKey('CareRecord.record_id'))
     def __repr__(self):
         return showItem(self)
 
 class Doctor(db.Model):
     __tablename__='Doctor'
-    id=db.Column(db.Integer,db.ForeignKey('Account.id'),primary_key=True)
-    account=db.relationship('Account')
-    
+    account_id=db.Column(db.Integer,db.ForeignKey('Account.account_id'),primary_key=True)
     name=db.Column(db.String)
     clinic=db.Column(db.String)
     contact=db.Column(db.String)
@@ -57,40 +60,32 @@ class Doctor(db.Model):
 
 class CareRecipient(db.Model):
     __tablename__='CareRecipient'
-    id=db.Column(db.Integer,db.ForeignKey('Account.id'),primary_key=True)
-    account=db.relationship('Account')
-    
+    account_id=db.Column(db.Integer,db.ForeignKey('Account.account_id'),primary_key=True)
     name=db.Column(db.String)
+    gender=db.Column(db.String)
     address=db.Column(db.String)
-    
     resident_contact=db.Column(db.String)
     mobile_contact=db.Column(db.String)
-    
-    contact_person_id=db.Column(db.Integer,db.ForeignKey('FamilyMember.id'))
-    
-    contact_person=db.relationship('FamilyMember',foreign_keys=[contact_person_id])
-    
-    doctor_id=db.Column(db.Integer,db.ForeignKey('Doctor.id'))
-    doctor=db.relationship('Doctor')
-    family_history=db.Column(db.String)
+    contact_person_id=db.Column(db.Integer,db.ForeignKey('FamilyMember.account_id'))
+    doctor_id=db.Column(db.Integer,db.ForeignKey('Doctor.account_id'))
     beacon_id=db.Column(db.String)
     def __repr__(self):
         return showItem(self)
 
 class FamilyMember(db.Model):
     __tablename__='FamilyMember'
-    id=db.Column(db.Integer,db.ForeignKey('Account.id'),primary_key=True)
-    account=db.relationship('Account')
-    
-    relationship=db.Column(db.String)
-    carerecipient=db.relationship('CareRecipient',backref='family_member')
+    account_id=db.Column(db.Integer,db.ForeignKey('Account.account_id'),primary_key=True)
+    name=db.Column(db.String)
+    address=db.Column(db.String)
+    contact_number=db.Column(db.String)
+
     def __repr__(self):
         return showItem(self)
     
 
 class Administrator(db.Model):
     __tablename__='Administrator'
-    id=db.Column(db.Integer,db.ForeignKey('Account.id'),primary_key=True)
+    id=db.Column(db.Integer,db.ForeignKey('Account.account_id'),primary_key=True)
     name=db.Column(db.String)
     account=db.relationship('Account')
     def __repr__(self):
@@ -99,16 +94,13 @@ class Administrator(db.Model):
 class CareRecord(db.Model):
     __tablename__='CareRecord'
     record_id=db.Column(db.Integer,primary_key=True)
-    caregiver_id=db.Column(db.Integer,db.ForeignKey('CareGiver.id'))
-    caregiver=db.relationship('CareGiver',backref='records',foreign_keys=[caregiver_id])
-    carerecipient_id=db.Column(db.Integer,db.ForeignKey('CareRecipient.id'))
-    carerecipient=db.relationship('CareRecipient',backref='records',foreign_keys=[carerecipient_id])
-
+    caregiver_id=db.Column(db.Integer,db.ForeignKey('CareGiver.account_id'))
+    caregiver_name=db.Column(db.String,db.ForeignKey('CareGiver.name'))
+    carerecipient_id=db.Column(db.Integer,db.ForeignKey('CareRecipient.account_id'))
+    carerecipient_name=db.Column(db.String,db.ForeignKey('CareRecipient.name'))
     appointment_time=db.Column(db.DATETIME)
     end_datetime=db.Column(db.DATETIME)
-    duration=db.Column(db.Integer)
 
-    location=db.Column(db.String)
     special_need=db.Column(db.String)
     record_status=db.Column(db.String)
     review=db.Column(db.String)
@@ -118,9 +110,9 @@ class CareRecord(db.Model):
 
 class DoctorReferral(db.Model):
     __tablename__='DoctorReferal'
-    id=db.Column(db.Integer,db.ForeignKey('Account.id'),primary_key=True)
-    doctor_id=db.Column(db.Integer,db.ForeignKey('Doctor.id'))
-    carerecipient_id=db.Column(db.Integer,db.ForeignKey('CareRecipient.id'))
+    referal_id=db.Column(db.Integer,primary_key=True)
+    doctor_id=db.Column(db.Integer,db.ForeignKey('Doctor.account_id'))
+    carerecipient_id=db.Column(db.Integer,db.ForeignKey('CareRecipient.account_id'))
     referral_reason=db.Column(db.String)
     referral_date=db.Column(db.DATE)
     def __repr__(self):
--- BeaconServer/BeaconServer/runserver.py
+++ BeaconServer/BeaconServer/runserver.py
@@ -6,4 +6,4 @@ from os import environ
 from BeaconServer import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=80,debug=True)
